f55cc2f7e2a24050bc154bb190e389ab69edad22
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-03-29 14:11:26 +0100

    recive data from registry update
    recive data from registry update

--- Server/server.c
+++ Server/server.c
@@ -5,29 +5,38 @@
 #include <stdio.h>
 #include "server.h"
 
-
-game sysRegistry(game gameData) {
+game FillRegistryValues(game gameData) {
 
     HKEY key, KeySuccess;
-    DWORD value;
+    DWORD dwValue;
+    WCHAR wcValue[TAM];
+    PVOID pvData = wcValue;
 
-    if (RegOpenKeyEx(HKEY_CURRENT_USER, N_TRACKS, 0, KEY_ALL_ACCESS, &key) == ERROR_SUCCESS) {
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, N_TRACKS, 0, KEY_READ, &key) == ERROR_SUCCESS) {
+           
+        DWORD size = sizeof(wcValue);
+        LONG err = RegGetValue(key, NULL, N_TRACKS_ATT, RRF_RT_ANY, NULL, pvData, &size);
 
-        _tprintf(TEXT("Retirar dados do numero de estradas\n"));
+        if (err == ERROR_SUCCESS) {
+            DWORD convert = *(DWORD*)pvData;
+            gameData.num_tracks = (INT)convert;
+        }else {
+            _tprintf(_T("[SERVER] ERRO ao receber os valores do registry !"));
+        }
 
     }else {
-
+        
         do {
-            _tprintf(_T("[SERVER] Insira o número de estradas (1-8) "));
-            _tscanf_s(_T("%u"), &value, sizeof(value));
-            gameData.num_tracks = (INT)value;
+            _tprintf(COUT_TRACKS);
+            _tscanf_s(_T("%u"), &dwValue, sizeof(dwValue));
+            gameData.num_tracks = (INT)dwValue;
 
-        } while (gameData.num_tracks > GAME_MAX_TRACKS || gameData.num_tracks < 1);
+        } while (gameData.num_tracks > (INT) GAME_MAX_TRACKS || gameData.num_tracks < (INT) 1);
 
-        if (RegCreateKeyEx(HKEY_CURRENT_USER, N_TRACKS, 0, NULL, REG_OPTION_VOLATILE,KEY_ALL_ACCESS, NULL, &key, NULL) == ERROR_SUCCESS) {
+        if (RegCreateKeyEx(HKEY_CURRENT_USER, N_TRACKS, 0, NULL, REG_OPTION_VOLATILE,KEY_WRITE, NULL, &key, NULL) == ERROR_SUCCESS) {
 
             if (RegOpenKeyEx(HKEY_CURRENT_USER, N_TRACKS, 0, KEY_ALL_ACCESS, &KeySuccess) == ERROR_SUCCESS) 
-                RegSetValueEx(KeySuccess, N_TRACKS_ATT, 0, REG_DWORD, &value, sizeof(value));
+                RegSetValueEx(KeySuccess, N_TRACKS_ATT, 0, REG_DWORD, &dwValue, sizeof(dwValue));
 
             RegCloseKey(KeySuccess);
         }
@@ -35,21 +44,32 @@ game sysRegistry(game gameData) {
     }
 
     RegCloseKey(key);
-
     return gameData;
 }
 
 int _tmain(int argc, TCHAR* argv[]) {
 
+    unicodeInitializer();
+    game gameData;
+    gameData = fillGameStruct();
+    gameData = FillRegistryValues(gameData);
+
+
+
+    return 0;
+}
+
+game fillGameStruct() {
+    game g;
+    g.num_tracks = 0;
+    g.vehicle_speed = 0;
+    return g;
+}
+
+void unicodeInitializer() {
 #ifdef UNICODE
     _setmode(_fileno(stdin), _O_WTEXT);
     _setmode(_fileno(stdout), _O_WTEXT);
     _setmode(_fileno(stderr), _O_WTEXT);
 #endif
-    game gameData;
-    gameData.num_tracks = 0;
-    gameData.vehicle_speed = 0;
-
-    gameData = sysRegistry(gameData);
-    return 0;
 }
--- Server/server.h
+++ Server/server.h
@@ -11,6 +11,7 @@
 #define TAM 40
 #define N_TRACKS TEXT("FroggerGame\\NumberTracks")
 #define N_TRACKS_ATT TEXT("TRACKS_VALUE")
+#define COUT_TRACKS TEXT("[SERVER] Insira o número de estradas (1-8) :")
 #define GAME_MAX_TRACKS 8
 
 #define START_SPEED TEXT("FroggerGame\\StartSpeed")
@@ -20,5 +21,9 @@ typedef struct game {
 	INT num_tracks;
 	INT vehicle_speed;
 }game;
+
+
+void unicodeInitializer();
+game fillGameStruct();
 
 #endif
