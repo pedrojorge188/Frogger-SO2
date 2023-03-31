e079065a384628091c2f16e81da1321584dbacea
Daniel Rodrigues <107119895+daniel2dd1@users.noreply.github.com>
2023-04-01 00:52:39 +0100

    aceitar comandos

--- Operator/operator.c
+++ Operator/operator.c
@@ -22,7 +22,27 @@ DWORD WINAPI input_thread(LPVOID lpParam) {
         if (_tscanf_s(_T("%s %d"), command, 50, &value) == 2) {
 
             if (wcscmp(command, _T("exit")) == 0) {
+
                 ExitThread(1);
+
+            }
+            else if (wcscmp(command, _T("stoptime")) == 0) {
+
+                if (value <= 0)
+                    _tprintf(_T("[SERVER] Valor Inválido!\n"));
+                else
+                    _tprintf(_T("[SERVER] Valor alterado!\n"));
+
+            }
+            else if (wcscmp(command, _T("obstacle")) == 0) {
+
+                _tprintf(_T("[SERVER] Obstaculo colocado!\n"));
+
+            }
+            else if (wcscmp(command, _T("invertdir")) == 0) {
+
+                _tprintf(_T("[SERVER] Direção invertida!\n"));
+
             }
 
         }
