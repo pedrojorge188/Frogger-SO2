a407746d4fdc7f512a0186e03d817c932b59866f
Daniel Rodrigues <107119895+daniel2dd1@users.noreply.github.com>
2023-04-01 00:06:22 +0100

    Frog instances

---
+++ Frog/Frog/frog.h
@@ -0,0 +1,25 @@
+#ifndef FROG_H
+#define FROG_H
+
+#include <windows.h>
+#include <tchar.h>
+#include <io.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#define TAM 40
+
+#define TIMEOUT 10000
+#define FROG_RUNNING_MSG TEXT("Já existe dois programas frog em atividade!")
+#define FROG_SEMAPHORE TEXT("FrogInstances")
+
+#define N_TRACKS TEXT("FroggerGame\\NumberTracks")
+
+#define START_SPEED TEXT("FroggerGame\\StartSpeed")
+
+#define MAX_FROGS 2
+
+
+void UNICODE_INITIALIZER();
+
+#endif
