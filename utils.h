bc202385576e67d1e6105e8c55cd6f6f15f49d4b
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-04-11 18:48:57 +0100

    PUSH

--- utils.h
+++ utils.h
@@ -10,5 +10,6 @@
 
 #define SERVER_SEMAPHORE TEXT("ServerInstances")
 #define SERVER_SHUTDOWN TEXT("ServerExit")
-
+#define SHARED_MEMORY_NAME TEXT("MEM_WITH_DATA")
+#define SHARED_MEMORY_SIZE 1024
 #endif
